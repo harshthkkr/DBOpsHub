cf72102bb1a3eabb1ab35f7db9e4945366670d41
Harsh Thakkar <72026921+harshthkkr@users.noreply.github.com>
2024-09-05 16:05:27 +0530

    specific plan

--- SQLServer/find_execution_plan_by_name.sql
+++ SQLServer/find_execution_plan_by_name.sql
@@ -9,3 +9,9 @@ SELECT top 10 *
 FROM sys.dm_exec_cached_plans AS cp 
 CROSS APPLY sys.dm_exec_sql_text(cp.plan_handle) AS st  
 WHERE st.TEXT LIKE '%<insert unique text here>%'
+
+--Get the execution plan from handle
+SELECT * FROM sys.dm_exec_query_plan (<plan handle from the first query>)
+
+-- Remove the specific plan from the cache.
+--DBCC FREEPROCCACHE (0x060006001ECA270EC0215D05000000000000000000000000);
